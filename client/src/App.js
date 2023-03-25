53e80b9ce18d3543aa905f8e2f62171d73737bc4
Jeemin Kim <cubbiekim@gmail.com>
2023-03-25 10:33:04 -0700

    added frontend + backend db integration complete (#5)
    * feat: added frontend
    
    * feat: added fake data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Diversity Project
-        </p>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+
+              // element={isAuth ? <HomePage /> : <Navigate to="/" />}
+              element={<HomePage />}
+            />
+            <Route
+              path="/profile/:userId"
+              // element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+              element={<ProfilePage />}
+
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
